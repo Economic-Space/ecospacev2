c728b864e61edebae3de0dc0212932598c2f3dfa
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-03-15 09:23:16 +0700

    feat: login register

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -55,15 +55,46 @@
 
             </ul>
             <ul class="navbar-nav mb-2 mb-lg-0">
+                @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                 {{-- <li class="nav-item px-2">
                     <a class="nav-link font-24" aria-current="page" href="{{url('/soon')}}">Sign Up</a>
                 </li>
                 <li class="nav-item px-2">
                     <a class="btn btn-dark font-24" aria-current="page" href="{{url('/soon')}}">Login</a>
                 </li> --}}
-                <li class="nav-item px-2">
+                {{-- <li class="nav-item px-2">
                     <a target="_blank" class="btn btn-dark font-24" aria-current="page" href="https://bit.ly/OprecTutorECONSPACE">Join Economic Space</a>
-                </li>
+                </li> --}}
             </ul>
           </div>
         </div>
