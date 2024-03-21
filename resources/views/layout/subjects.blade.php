2d2f3e002b2c339b8151a3a395628c5e9cfb45c8
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-03-21 10:03:08 +0700

    feat: rapiin ui login

--- resources/views/layout/subjects.blade.php
+++ resources/views/layout/subjects.blade.php
@@ -3,19 +3,19 @@
 
     <div class="col-12 col-md-4 card-tutor">
         <a href="{{ URL::route('detailTutor', $s->id) }}">
-            <div style="background-image: red; display: block; position: relative;">
+            <div class="block-relative">
                 {{-- <img  src="{{asset('assets/thumbnail/'.$s->subject_image)}}" alt="{{$s->subject_image}}" class="img-fluid w-100 desktop">
                 <img  src="{{asset('assets/thumbnailMobile/'.$s->subject_image)}}" alt="{{$s->subject_image}}" class="img-fluid w-100 mobile"> --}}
 
-                <img src="{{asset('assets/tutorImage/tDesktop.svg')}}" alt="image course" class="w-100 img-fluid desktop" style="position: relative;">
-                <img src="{{asset('assets/tutorImage/tMobile.svg')}}" alt="image course" class="w-100 img-fluid mobile" style="position: relative;">
+                <img src="{{asset('assets/tutorImage/tDesktop.svg')}}" alt="image course" class="w-100 img-fluid desktop pos-relative">
+                <img src="{{asset('assets/tutorImage/tMobile.svg')}}" alt="image course" class="w-100 img-fluid mobile pos-relative">
 
-                <h1 style="position: absolute; display: inline; z-index: 1; top: 35%; left: 35px; max-width: 40%; text-align: center" class="font-22 font-700 bg-white p-1">
+                <h1 class="font-22 font-700 bg-white p-1 subject-title">
                     {{$s->subject_title}}
                 </h1>
 
 
-                <img src="{{asset('assets/tutorImage/'.$s->subject_tutor)}}" alt="" style="position: absolute; right: 15px; bottom: 11px; height: 90%; z-index: 1;">
+                <img src="{{asset('assets/tutorImage/'.$s->subject_tutor)}}" alt="" class="subject-image">
 
             </div>
 
